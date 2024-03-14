ed83370844fe6b2f0085dcf6d86f52e885dc6bbc
Seenuvasan <2141853@cognizant.com>
2024-03-14 15:04:30 +0530

    connecting to database

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"io/ioutil"
+	"lms/db"
+	"log"
+)
+
+func main(){
+	//connecting to databse
+	DB,err:=db.InitDB()
+	if err!=nil{
+		log.Fatal("error in connecting")
+	}
+	defer DB.Close()
+	script,err:=ioutil.ReadFile("init.sql")
+	if err!=nil{
+		//fmt.Println(err)
+		log.Fatal(err)
+	}
+	_,err=DB.Exec(string(script))
+	if err!=nil{
+		log.Fatal(err)
+	}
+	
+
+}
